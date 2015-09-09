cdc0b8b099ce50991f4ad1a3a356c4fd5cd95b35
Jack Dunger <jack.dunger@physics.ox.ax.uk>
2015-09-09 14:38:20 +0100

    direct bin find

--- src/pdf/binned/PdfAxis.cpp
+++ src/pdf/binned/PdfAxis.cpp
@@ -24,9 +24,10 @@ PdfAxis::PdfAxis(const std::string& name_, double min_, double max_, size_t nBin
 }
 
 size_t PdfAxis::FindBin(double value_) const{
-    size_t insertIndex = std::lower_bound(fBinHighEdges.begin(), fBinHighEdges.end(), value_) - 
-        fBinHighEdges.begin();
-    if (insertIndex == fNBins)
-        return insertIndex-1;
+    size_t insertIndex = (size_t)((value_ - fBinLowEdges[0])/fBinWidth);
+    if (insertIndex)
+        return 0;
+    if (insertIndex > fNBins -1)
+        return fNBins -  1;
     return insertIndex;
 }
